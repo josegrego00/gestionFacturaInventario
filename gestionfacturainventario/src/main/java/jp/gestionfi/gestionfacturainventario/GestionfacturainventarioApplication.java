f022ba0475ad69871eca96aa74143ee6d226f2bc
Jose Pino <josepinodev@gmail.com>
2025-11-01 10:03:13 -0500

    Creacion de proyecto maven, spring boot y java

---
+++ gestionfacturainventario/src/main/java/jp/gestionfi/gestionfacturainventario/GestionfacturainventarioApplication.java
@@ -0,0 +1,13 @@
+package jp.gestionfi.gestionfacturainventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionfacturainventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionfacturainventarioApplication.class, args);
+	}
+
+}
---
+++ gestionfacturainventario/src/test/java/jp/gestionfi/gestionfacturainventario/GestionfacturainventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.gestionfi.gestionfacturainventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionfacturainventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
